0c72d6b6592bddbc21aa896af088468dabb7dd30
Constantin Koch <constantin.koch2000@gmail.com>
2020-03-21 06:44:45 +0100

    Started implementing better List, still to finish.

--- App/wirvsvirus/lib/JobOverview.dart
+++ App/wirvsvirus/lib/JobOverview.dart
@@ -10,11 +10,37 @@ class JobOverview extends StatelessWidget {
       itemCount: jobs.length,
       itemBuilder: (context, index) {
         final job = this.jobs[index];
+        // return JobListTile(job);
         return ListTile(
           title: Text(job.title),
-          subtitle: Text('Employer: ' + job.employer + ' Donation: ' + job.donation.toString() + ''),
+          subtitle: Text('Employer: ' + job.employer + ', Donation: ' + job.donation.toString()),
         );
       }
     );
   }
 }
+
+//ToDO fertig implementieren
+/*
+class JobListTile extends ListTile {
+  final Jobs job;
+
+  JobListTile(this.job);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      primary: false,
+      padding: EdgeInsets.all(20.0),
+      crossAxisSpacing: 100,
+      mainAxisSpacing: 10,
+      children: <Widget>[
+        Container(
+
+        )
+      ],
+    );
+  }
+}
+*/
