cf3c0d1c6a8f059bed14c74b4e4b87b75b52ac7c
Chris <christian_schreier-1500@gmx.de>
2020-03-22 03:24:10 +0100

    Zwischenspeicherung Popup-Versuch

--- Frontend/controller/homeController.js
+++ Frontend/controller/homeController.js
@@ -1,7 +1,20 @@
-﻿app.controller('homeController', ['$scope', '$interval', function ($scope, $interval) {
+﻿app.controller('homeController', ['$scope', '$document', function ($scope, $document) {
 
     componentHandler.upgradeAllRegistered();
 
 
+    $scope.jobOffers = [];
+
+    $scope.jobOffers.push({ icon: 'domain', title : 'Regale einräumen', employer: 'Rewe', donation: '20,0'});
+    $scope.jobOffers.push({ icon: 'person', title : 'Einkäufe erledigen', employer: 'Oma Hilde', donation: '0,00'});
+    $scope.jobOffers.push({ icon: 'domain', title : 'Klopapierkäufer kontrollieren', employer: 'Aldi', donation: '10,00'});
+
+
+    $scope.jobSelected = function() {
+        console.log("geht");
+        var popup = document.getElementById("myPopup");
+        console.log(popup);
+        popup.classList.toggle("show");
+      }
 
 }]);
