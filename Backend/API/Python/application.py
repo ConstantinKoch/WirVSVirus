b5193feb2c30a1850b9993ae2daad52da7cc7ec2
Constantin Koch <constantin.koch2000@gmail.com>
2020-03-22 10:50:38 +0100

    Added /user and /job

--- Backend/API/Python/application.py
+++ Backend/API/Python/application.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, request
 from flask_json import FlaskJSON, JsonError, json_response, as_json
-from datetime import datetime
 
 # print a nice greeting.
 def say_hello(username = "World"):
@@ -14,10 +13,23 @@ Json = FlaskJSON(application)
 def hello():
     return json_response(text = "Herzlich Willkommen zur DonateYourWork API! :)")
 
-@application.route('/get_time')
-def get_time():
-    now = datetime.utcnow()
-    return json_response(time=now)
+@application.route("/job", methods=["POST","GET"])
+def job():
+    if request.method == "GET":
+        title = request.args.get("title")
+        employer = request.args.get("employer")
+        donation = request.args.get("donation")
+        return "It worked"
+    elif request.method == "POST":
+        return "It works"
+
+@application.route("/user", methods=["GET"])
+def user():
+    username = request.args.get("username")
+    email = request.args.get("email")
+    description = request.args.get("description")
+    location = request.args.get("location")
+    return "user"
 
 # run the app.
 if __name__ == "__main__":
